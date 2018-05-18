ffba517cc012fa6f592e933ef7cb3a669e4372ba
Luca Aquino <luca-aquino@owndays.co.jp>
2018-05-18 12:02:55 +0900

    renamed mainactivity to homeactivity

--- app/src/main/java/lu/aqu/projper/ui/home/HomeActivity.java
+++ app/src/main/java/lu/aqu/projper/ui/home/HomeActivity.java
@@ -1,15 +1,15 @@
 package lu.aqu.projper.ui.home;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import lu.aqu.projper.R;
 
-public class MainActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home);
     }
 }
