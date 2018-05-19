b886b34992bfbf7db0ab41151706be1a39114238
Luca Aquino <akku@outlook.com>
2018-05-19 12:13:52 +0900

    removed unused variable

--- app/src/main/java/lu/aqu/projper/ui/home/HomeActivity.java
+++ app/src/main/java/lu/aqu/projper/ui/home/HomeActivity.java
@@ -2,16 +2,10 @@ package lu.aqu.projper.ui.home;
 
 import android.os.Bundle;
 
-import javax.inject.Inject;
-
 import lu.aqu.projper.R;
-import lu.aqu.projper.api.endpoint.ProjectService;
 import lu.aqu.projper.ui.BaseActivity;
 
 public class HomeActivity extends BaseActivity<HomePresenter> {
-
-    @Inject
-    ProjectService projectService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
