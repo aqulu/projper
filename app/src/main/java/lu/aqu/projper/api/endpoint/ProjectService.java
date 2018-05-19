aa5b409f25ef201341bfb2cc32b926ac2608aaec
Luca Aquino <akku@outlook.com>
2018-05-19 12:16:56 +0900

    load project data from ghpages test feed

--- app/src/main/java/lu/aqu/projper/api/endpoint/ProjectService.java
+++ app/src/main/java/lu/aqu/projper/api/endpoint/ProjectService.java
@@ -4,20 +4,17 @@ import java.util.List;
 
 import io.reactivex.Observable;
 import lu.aqu.projper.model.Project;
-import retrofit2.http.Body;
 import retrofit2.http.GET;
-import retrofit2.http.POST;
-import retrofit2.http.Path;
 
 public interface ProjectService {
 
-    @GET("projects")
+    @GET("projects.json")
     Observable<List<Project>> list();
 
-    @GET("projects/{id}")
-    Observable<Project> get(@Path("id") long id);
+//    @GET("projects/{id}")
+//    Observable<Project> get(@Path("id") long id);
 
-    @POST("projects")
-    Observable<Project> create(@Body Project project);
+//    @POST("projects")
+//    Observable<Project> create(@Body Project project);
 
 }
