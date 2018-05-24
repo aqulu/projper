c38f0e4a423bc55d9e27aac1ea179e955dcc281e
Luca Aquino <luca-aquino@owndays.co.jp>
2018-05-24 11:26:20 +0900

    made project parcelable

--- app/src/main/java/lu/aqu/projper/model/Project.java
+++ app/src/main/java/lu/aqu/projper/model/Project.java
@@ -1,10 +1,13 @@
 package lu.aqu.projper.model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import com.google.gson.annotations.SerializedName;
 
 import java.util.List;
 
-public class Project {
+public class Project implements Parcelable {
 
     @SerializedName("id")
     private long id;
@@ -14,6 +17,38 @@ public class Project {
     private String description;
     @SerializedName("tags")
     private List<String> tags;
+
+    protected Project(Parcel in) {
+        id = in.readLong();
+        name = in.readString();
+        description = in.readString();
+        tags = in.createStringArrayList();
+    }
+
+    public static final Creator<Project> CREATOR = new Creator<Project>() {
+        @Override
+        public Project createFromParcel(Parcel in) {
+            return new Project(in);
+        }
+
+        @Override
+        public Project[] newArray(int size) {
+            return new Project[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeLong(id);
+        parcel.writeString(name);
+        parcel.writeString(description);
+        parcel.writeStringList(tags);
+    }
 
     public long getId() {
         return id;
