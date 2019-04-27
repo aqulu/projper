7dc041075fb02319cce16fac501731359e3d71b6
aqulu <dev@aqu.lu>
2019-04-27 23:19:49 +0900

    create nav_graph in project module, use as start destination in app

--- modules/features/project/project/src/main/java/lu/aqu/projper/project/OverviewFragment.kt
+++ modules/features/project/project/src/main/java/lu/aqu/projper/project/OverviewFragment.kt
@@ -1,15 +1,14 @@
-package lu.aqu.projper.ui
+package lu.aqu.projper.project
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import lu.aqu.projper.R
 
-class HomeFragment : Fragment() {
+class OverviewFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return inflater.inflate(R.layout.fragment_overview, container, false)
     }
 }
