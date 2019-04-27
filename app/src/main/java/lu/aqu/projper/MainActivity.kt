8549017f1209dce8baaa900d8e7db9a6a22b7e14
Luca Aquino <me@aqu.lu>
2019-04-27 13:52:45 +0900

    change to androidx and fix gradle dependencies

--- app/src/main/java/lu/aqu/projper/MainActivity.kt
+++ app/src/main/java/lu/aqu/projper/MainActivity.kt
@@ -1,7 +1,7 @@
 package lu.aqu.projper
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
