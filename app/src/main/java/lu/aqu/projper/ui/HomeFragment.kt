c3bf8642ac1430b49efb4d9f1bd21bd9f6a049f3
Luca Aquino <me@aqu.lu>
2019-04-27 14:26:04 +0900

    use NavHostFragment in MainActivity

---
+++ app/src/main/java/lu/aqu/projper/ui/HomeFragment.kt
@@ -0,0 +1,15 @@
+package lu.aqu.projper.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import lu.aqu.projper.R
+
+class HomeFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+}
