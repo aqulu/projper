82fdc1fadd8a965120341d87196df2677fe78b21
aqulu <dev@aqu.lu>
2019-05-11 09:29:28 +0900

    create DiffUtil.ItemCallback for entities

--- modules/features/project/project/src/main/java/lu/aqu/projper/project/ProjectAdapter.kt
+++ modules/features/project/project/src/main/java/lu/aqu/projper/project/ProjectAdapter.kt
@@ -2,13 +2,13 @@ package lu.aqu.projper.project
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import lu.aqu.projper.project.databinding.ViewHolderProjectBinding
 import lu.aqu.projper.project.domain.Project
+import lu.aqu.projper.support.EntityDiffCallback
 
-class ProjectAdapter : ListAdapter<Project, ProjectAdapter.ViewHolder>(itemCallback) {
+class ProjectAdapter : ListAdapter<Project, ProjectAdapter.ViewHolder>(EntityDiffCallback()) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         return ViewHolder(parent)
@@ -33,16 +33,6 @@ class ProjectAdapter : ListAdapter<Project, ProjectAdapter.ViewHolder>(itemCallb
 
         fun bind(project: Project) {
             binding.project = project
-        }
-    }
-
-    companion object {
-        private val itemCallback = object : DiffUtil.ItemCallback<Project>() {
-            override fun areItemsTheSame(oldItem: Project, newItem: Project): Boolean =
-                oldItem.id == newItem.id
-
-            override fun areContentsTheSame(oldItem: Project, newItem: Project): Boolean =
-                oldItem == newItem
         }
     }
 }
