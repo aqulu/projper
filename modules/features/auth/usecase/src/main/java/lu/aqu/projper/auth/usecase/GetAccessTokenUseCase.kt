716361ecb1cd404acd2a8699af594bacae9cad88
aqulu <dev@aqu.lu>
2019-06-01 01:13:30 +0900

    feat auth: create getAccessToken usecase

---
+++ modules/features/auth/usecase/src/main/java/lu/aqu/projper/auth/usecase/GetAccessTokenUseCase.kt
@@ -0,0 +1,5 @@
+package lu.aqu.projper.auth.usecase
+
+import lu.aqu.core.coroutine.CoroutineUseCase
+
+interface GetAccessTokenUseCase : CoroutineUseCase<String?>
--- modules/features/auth/usecase/src/main/java/lu/aqu/projper/auth/usecase/UseCaseModule.kt
+++ modules/features/auth/usecase/src/main/java/lu/aqu/projper/auth/usecase/UseCaseModule.kt
@@ -5,6 +5,7 @@ import dagger.Provides
 import kotlinx.coroutines.Dispatchers
 import lu.aqu.projper.auth.domain.AuthRepository
 import lu.aqu.projper.auth.infra.InfraModule
+import lu.aqu.projper.auth.usecase.impl.GetAccessTokenUseCaseImpl
 import lu.aqu.projper.auth.usecase.impl.LoginUseCaseImpl
 
 @Module(
@@ -15,4 +16,8 @@ class UseCaseModule {
     @Provides
     fun provideLoginUseCase(authRepository: AuthRepository): LoginUseCase =
         LoginUseCaseImpl(Dispatchers.Main, Dispatchers.IO, authRepository)
+
+    @Provides
+    fun provideGetAccessTokenUseCase(authRepository: AuthRepository): GetAccessTokenUseCase =
+        GetAccessTokenUseCaseImpl(Dispatchers.Main, Dispatchers.Default, authRepository)
 }
