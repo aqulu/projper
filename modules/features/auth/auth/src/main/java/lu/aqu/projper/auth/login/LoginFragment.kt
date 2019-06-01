0e92e9240ce8978edce0631dbf2c2e67bad7104b
aqulu <dev@aqu.lu>
2019-06-01 10:33:35 +0900

    feat login: implement login viewmodel and ui behavior

--- modules/features/auth/auth/src/main/java/lu/aqu/projper/auth/login/LoginFragment.kt
+++ modules/features/auth/auth/src/main/java/lu/aqu/projper/auth/login/LoginFragment.kt
@@ -5,8 +5,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.fragment.findNavController
+import lu.aqu.core.support.Resource
+import lu.aqu.projper.auth.R
 import lu.aqu.projper.auth.databinding.FragmentLoginBinding
 import lu.aqu.projper.auth.login.di.LoginComponent
 import lu.aqu.projper.di.componentHolder
@@ -33,4 +38,32 @@ class LoginFragment : Fragment() {
         FragmentLoginBinding.inflate(inflater, container, false)
             .also { binding = it }
             .root
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel.loginResult.observe(this, Observer {
+            binding.isLoading = it is Resource.Loading
+            when (it) {
+                is Resource.Success -> {
+                    Toast.makeText(
+                        requireContext(),
+                        getString(R.string.login_greeting, it.data.email),
+                        Toast.LENGTH_LONG
+                    ).show()
+                    findNavController().popBackStack()
+                }
+
+                is Resource.Error -> {
+                    Toast.makeText(requireContext(), R.string.login_error, Toast.LENGTH_LONG).show()
+                }
+            }
+        })
+
+        binding.apply {
+            login.setOnClickListener {
+                viewModel.login(email.text.toString(), password.text.toString())
+            }
+        }
+    }
 }
--- modules/features/auth/auth/src/main/java/lu/aqu/projper/auth/login/LoginViewModel.kt
+++ modules/features/auth/auth/src/main/java/lu/aqu/projper/auth/login/LoginViewModel.kt
@@ -1,15 +1,42 @@
 package lu.aqu.projper.auth.login
 
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.*
+import kotlinx.coroutines.launch
+import lu.aqu.core.coroutine.ControlledRunner
+import lu.aqu.core.support.Resource
+import lu.aqu.projper.auth.domain.User
+import lu.aqu.projper.auth.usecase.LoginQuery
 import lu.aqu.projper.auth.usecase.LoginUseCase
 import javax.inject.Inject
 
 class LoginViewModel private constructor(
-    private val loginUseCase: LoginUseCase
+    loginUseCase: LoginUseCase
 ) : ViewModel() {
 
-    // TODO implement login
+    /**
+     * login usecase chain emitting results to [mutableLoginResult]
+     */
+    private val loginAction = loginUseCase
+        .onLoading { mutableLoginResult.value = Resource.Loading }
+        .onError { mutableLoginResult.value = Resource.Error(it) }
+        .onResult { mutableLoginResult.value = Resource.Success(it) }
+
+    /**
+     * concurrency helper to avoid multiple login at once
+     */
+    private val controlledRunner = ControlledRunner<Unit>()
+
+    private val mutableLoginResult = MutableLiveData<Resource<User>>()
+    val loginResult = mutableLoginResult
+
+    fun login(email: String, password: String): LiveData<Resource<User>> {
+        viewModelScope.launch {
+            controlledRunner.cancelPreviousThenRun {
+                loginAction.invoke(LoginQuery(email, password))
+            }
+        }
+        return loginResult
+    }
 
     class Factory @Inject constructor(
         private val loginUseCase: LoginUseCase
