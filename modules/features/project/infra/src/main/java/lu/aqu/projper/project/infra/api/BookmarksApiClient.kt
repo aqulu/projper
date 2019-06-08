048009359dba08b438c1703b9f73135a949a22f5
aqulu <dev@aqu.lu>
2019-06-08 10:17:07 +0900

    fix: bookmarks api http method annotations

--- modules/features/project/infra/src/main/java/lu/aqu/projper/project/infra/api/BookmarksApiClient.kt
+++ modules/features/project/infra/src/main/java/lu/aqu/projper/project/infra/api/BookmarksApiClient.kt
@@ -3,21 +3,19 @@ package lu.aqu.projper.project.infra.api
 import kotlinx.coroutines.Deferred
 import lu.aqu.projper.project.infra.api.json.AddBookmarkQueryJson
 import lu.aqu.projper.project.infra.api.json.ProjectJson
-import retrofit2.http.Body
-import retrofit2.http.GET
-import retrofit2.http.Path
+import retrofit2.http.*
 
 internal interface BookmarksApiClient {
 
     @GET("bookmarks")
     fun findAllAsync(): Deferred<List<ProjectJson>>
 
-    @GET("bookmarks")
+    @POST("bookmarks")
     fun addAsync(
         @Body addBookmarkQueryJson: AddBookmarkQueryJson
     ): Deferred<ProjectJson>
 
-    @GET("bookmarks/{id}")
+    @DELETE("bookmarks/{id}")
     fun removeAsync(
         @Path("id") id: Long
     ): Deferred<ProjectJson>
