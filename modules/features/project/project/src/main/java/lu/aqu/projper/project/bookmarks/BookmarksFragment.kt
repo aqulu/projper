21fd57f7b997b7d7e4ac05fb5d9c16082db889a1
aqulu <dev@aqu.lu>
2019-06-13 01:54:27 +0900

    feat auth: check authstate in bookmarks fragment with shared viewmodel

--- modules/features/project/project/src/main/java/lu/aqu/projper/project/bookmarks/BookmarksFragment.kt
+++ modules/features/project/project/src/main/java/lu/aqu/projper/project/bookmarks/BookmarksFragment.kt
@@ -10,23 +10,29 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.findNavController
 import kotlinx.android.synthetic.main.fragment_bookmarks.bookmarksRecyclerView
 import lu.aqu.core.support.Resource
+import lu.aqu.core.util.Constants
 import lu.aqu.projper.di.componentHolder
 import lu.aqu.projper.project.ProjectComponent
 import lu.aqu.projper.project.R
 import lu.aqu.projper.project.common.ProjectAdapter
 import lu.aqu.projper.project.details.DetailsFragmentArgs
-import retrofit2.HttpException
+import lu.aqu.projper.ui.AuthViewModel
 import javax.inject.Inject
 
 class BookmarksFragment : Fragment() {
 
     @Inject
     lateinit var viewModelFactory: BookmarkViewModel.Factory
-
     private val viewModel: BookmarkViewModel by viewModels { viewModelFactory }
+    private val authViewModel: AuthViewModel by lazy {
+        ViewModelProviders
+            .of(requireActivity())
+            .get(Constants.AUTH_VIEW_MODEL, AuthViewModel::class.java)
+    }
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -39,6 +45,12 @@ class BookmarksFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        authViewModel.authenticationState.observe(this, Observer {
+            if (it == AuthViewModel.AuthenticationState.UNAUTHENTICATED) {
+                findNavController().navigate(R.id.loginDest)
+            }
+        })
 
         val adapter = ProjectAdapter {
             findNavController().navigate(
@@ -56,22 +68,10 @@ class BookmarksFragment : Fragment() {
                 }
 
                 is Resource.Error -> {
-                    onError(it.throwable)
+                    Log.w("Bookmarks", it.throwable)
+                    Toast.makeText(context, R.string.error_data_fetching, Toast.LENGTH_LONG).show()
                 }
             }
         })
-    }
-
-    private fun onError(throwable: Throwable) {
-        when (throwable) {
-            is HttpException -> {
-                // TODO check if http exception is 403
-                findNavController().navigate(R.id.loginDest)
-            }
-            else -> {
-                Log.w("Bookmarks", throwable)
-                Toast.makeText(context, R.string.error_data_fetching, Toast.LENGTH_LONG).show()
-            }
-        }
     }
 }
